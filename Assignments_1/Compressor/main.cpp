70d6c5d5e6242f5a4206b5558a13d0848369671e
Ahmed El Badry <ahmed2lbadry@gmail.com>
2018-02-25 20:10:39 +0200

    CR.92

--- Assignments_1/Compressor/main.cpp
+++ Assignments_1/Compressor/main.cpp
@@ -10,13 +10,15 @@ int main()
 
     //bool flag = 0;
     //stack <string> st;
-    bitset<4> four;
+    bitset<3> three;
     bitset<6> six;
     bitset<8> eight;
     string s,t1,t0;
-    vector <string> first,second,third;
+    vector <string> first,second,thirdones,thirdzeros,third;
+    //vector <int> third0[10000], third1[10000];
     unordered_map <char , int> m;
-    int p = 1, c1 , c0;
+    int p = 1, c1 , c0, /*ma1 = 0, ma0 = 0,tmp0 = 0 , tmp1 = 0 ,*/ c;
+    unsigned int mi;
 
     for(char i = 'a' ; i <= 'z' ; i++)
         m[i] = p++;
@@ -30,6 +32,8 @@ int main()
 
     //1st step
     while(getline(cin , s)){
+        if(s.empty())
+            continue;
         t1 = "";
         for(int i = 0 ; i < (int)s.size() ; i++){
             if(i && s[i] == '/' && s[i-1] == '<'){
@@ -41,8 +45,8 @@ int main()
         first.push_back(t1);
     }
 
-    for(auto it : first)
-        cout<<it<<endl;
+    //for(auto it : first)
+    //    cout<<it<<endl;
 
     //2nd step
     for(int i = 0 ; i < (int)first.size() ; i++){
@@ -65,13 +69,85 @@ int main()
         second.push_back(t1);
     }
 
-    for(auto it : second)
-        cout<<it<<endl;
+    //for(auto it : second)
+    //    cout<<it<<endl;
 
     //3rd step
+    for(int i = 0 ; i < (int)second.size() ; i++){
+        t1 = t0 = "";
+        c1 = c0 = 0;
+        for(int j = 0 ; j < (int)second[i].size() ; j++){
+            if(second[i][j] == '0'){
+                c0++;
+                if(c1){
+                    t1 += '1';
+                    while(c1 >= 7){
+                        three = 7;
+                        t1 += three.to_string();
+                        c1 -= 7;
+                    }
+                    three = c1;
+                    t1 += three.to_string();
+                    c1 = 0;
+                }
+                else
+                    t1 += '0';
+                //ma1 = max(ma1 , c1);
+                //third1[i].push_back(c1);
+            }
+            else{
+                c1++;
+                if(c0){
+                    t0 += '1';
+                    while(c0 >= 7){
+                        three = 7;
+                        t0 += three.to_string();
+                        c0 -= 7;
+                    }
+                    three = c0;
+                    t0 += three.to_string();
+                    c0 = 0;
+                }
+                else
+                    t0 += '0';
+                //ma0 = max(ma0 , c0);
+                //third0[i].push_back(c0);
+            }
+        }
+        /*for(auto it : third1[i])
+            cout<<it<<" ";
+        cout<<endl;
+        for(auto it : third0[i])
+            cout<<it<<" ";
+        cout<<endl<<endl;*/
+        //thirdones.push_back(t1);
+        //thirdzeros.push_back(t0);
 
+        mi = min(second[i].size() , min(t0.size() , t1.size()));
 
+        if(mi == second[i].size())
+            third.push_back("00" + second[i]);
+        else if(mi == t1.size())
+            third.push_back("01" + t1);
+        else
+            third.push_back("11" + t0);
+        //cout<<third[i]<<endl;
+    }
 
+    //last step
+    for(int i = 0 ; i<(int)third.size() ; i++){
+        cout<<(int)third[i].size() % 6;
+        for(int j = 0 ; j < (int)third[i].size() ;){
+            c = 0;
+            for(int k = (1LL<<min(5 , ((int)third[i].size() - j - 1))) ; k ; k/=2 , j++)
+                c += k * (third[i][j] == '1');
+            if(c > 9)
+                cout<<(char)(c - 10 + 33);
+            else
+                cout<<c;
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DataCompressionClass
+
+
+### Team
+Ahmed El Badry - Khaled Mohamed - Remon Fawzy
+
+### -
+
+
+## Assingment_1
+HTML code compression using RLE on bit level
+
